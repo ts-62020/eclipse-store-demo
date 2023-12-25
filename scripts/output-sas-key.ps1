a8f9fc38a0dbf6f42b9e66f04dafdd54049e4788
Torsten Schmischke <torsten.schmischke@adesso.de>
2023-12-25 01:48:01 +0100

    Add sas key properties to output package

--- scripts/output-sas-key.ps1
+++ scripts/output-sas-key.ps1
@@ -2,13 +2,23 @@
 [CmdletBinding()]
 param(
 	[string] $Path = ".",
-	[string] $FileName = "StorageAccountKey.txt"
+	[string] $FileName = "StorageAccountKey.properties",
+	[string] $TerraformTemplatePath = "$PSScriptRoot/../terraform"
 )
+
+Push-Location -Path $TerraformTemplatePath
 
 $key = terraform output -raw storage_account_key
 
+Pop-Location
+
+$fileContent = @"
+storage-filesystem.azure.storage.credentials.type=shared-key
+storage-filesystem.azure.storage.credentials.account-key=$key
+"@
+
 if ($key) {
-	New-Item -Path $Path -Name $FileName -ItemType "file" -Value $key -Force
+	New-Item -Path $Path -Name $FileName -ItemType "file" -Value $fileContent -Force
 } else {
 	throw "Key is empty"
 }
